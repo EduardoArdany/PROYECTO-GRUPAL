41f11a3059e6fe93470c9febf53657d57059b672
Angela <Angela@LAPTOP-8P28C5SB>
2023-02-21 00:54:18 -0600

    Hola ya esta

--- src/pckControlador/menuControladores.java
+++ src/pckControlador/menuControladores.java
@@ -1,7 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package pckControlador;
 
 import java.awt.event.ActionEvent;
@@ -23,6 +20,9 @@ public class menuControladores implements ActionListener{
     Reparacion VistaReparacion;
     ModelAgregar AgregarModel;
     public DefaultTableModel TablaEquipos = new DefaultTableModel();
+    public DefaultTableModel TablaPropietarios = new DefaultTableModel();
+    public DefaultTableModel TablaReparacion = new DefaultTableModel();
+    public DefaultTableModel TablaEntrega = new DefaultTableModel();
     
     public menuControladores(Interfaz VistaMenu,Equipos VistaEquipos, Entrega VistaEntrega,  
             Propietarios VistaPropietarios, Reparacion VistaReparacion, ModelAgregar AgregarModel){
@@ -59,6 +59,7 @@ public class menuControladores implements ActionListener{
     
     @Override
     public void actionPerformed(ActionEvent e) {
+        //Llevar del fomulario menu al de equipos
         if(e.getSource()==this.VistaMenu.btnIngresoEquipo){
             
             //PONER A LA ESCUCHA LOS BOTONES DE LA VISTA USUARIOS
@@ -77,10 +78,55 @@ public class menuControladores implements ActionListener{
             this.AgregarModel.ListaEquipos.get(0).getTipo(),
             this.AgregarModel.ListaEquipos.get(0).getProblema()});
         }
-    }
-    
-    //Levantar el formulario principal 
-    
-    
+        if(e.getSource()==this.VistaMenu.btnRegistroPropietario){
+            
+            this.VistaPropietarios.btnPropietario.addActionListener(this);
+            
+            this.VistaPropietarios.setLocationRelativeTo(null);
+            this.VistaPropietarios.setVisible(true);
+        }
+        if(e.getSource()==this.VistaPropietarios.btnPropietario){
+            this.AgregarModel.IngresarPropietarios(this.VistaPropietarios.txtApellidos.getText(),
+                    this.VistaPropietarios.txtNombre.getText(),
+                    this.VistaPropietarios.txtTelefono.getText());
+            this.TablaPropietarios.addRow(new Object[]{this.AgregarModel.ListaPropietarios.get(0).getApellidosP(),
+            this.AgregarModel.ListaPropietarios.get(0).getNombreP(),
+            this.AgregarModel.ListaPropietarios.get(0).getTelefono()});
+        }          
+        if(e.getSource()==this.VistaMenu.btnReparacion){
+            
+            this.VistaReparacion.btnReparacion.addActionListener(this);
+            
+            this.VistaReparacion.setLocationRelativeTo(null);
+            this.VistaReparacion.setVisible(true);
+        }
+        if(e.getSource()==this.VistaReparacion.btnReparacion){
+            this.AgregarModel.IngresarReparaciones(this.VistaReparacion.txtCodigo.getText(),
+                    this.VistaReparacion.txtDescripcion.getText(),
+                    this.VistaReparacion.txtFecha.getText(),
+                    this.VistaReparacion.txtTecnico.getText());
+            this.TablaReparacion.addRow(new Object[]{this.AgregarModel.ListaReparaciones.get(0).getCodigoR(),
+                this.AgregarModel.ListaReparaciones.get(0).getDescripcionR(),
+                this.AgregarModel.ListaReparaciones.get(0).getFechaR(),
+                this.AgregarModel.ListaReparaciones.get(0).getTecnico()});
+        }
+        if(e.getSource()==this.VistaEntrega.btnEntregar){
+            
+            this.VistaEntrega.btnEntregar.addActionListener(this);
+            
+            this.VistaEntrega.setLocationRelativeTo(null);
+            this.VistaEntrega.setVisible(true);
+        }
+        if(e.getSource()==this.VistaEntrega.btnEntregar){
+            this.AgregarModel.IngresarEntregas(this.VistaReparacion.txtCodigo.getText(),
+                    this.VistaReparacion.txtDescripcion.getText(),
+                    this.VistaReparacion.txtFecha.getText(),
+                    this.VistaReparacion.txtTecnico.getText());
+            this.TablaEntrega.addRow(new Object[]{this.AgregarModel.ListaEntregas.get(0).getCodigo(),
+                this.AgregarModel.ListaEntregas.get(0).getDescripcion(),
+                this.AgregarModel.ListaEntregas.get(0).getFecha(),
+                this.AgregarModel.ListaEntregas.get(0).getNombre()});
+        }
+    }    
 }
 //
